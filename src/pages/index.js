45dc4ea620a4afef46383f04ca47b4fac30197cf
double Z <doubleZ0108@163.com>
2020-04-29 17:02:27 +0800

    :art:component and props

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,14 @@ import Link from "gatsby-link"
 import '../layouts/index.css'
 
 import Header from "../components/header.js"
+import Card from "../components/card.js"
+
+
+import figmalogo from "../images/logo-figma.png"
+import framerlogo from "../images/logo-framer.png"
+import invisionlogo from "../images/logo-invision.png"
+import sketchlogo from "../images/logo-sketch.png"
+import swiftlogo from "../images/logo-swift.png"
 
 // export default () => <div>Hello world!</div>
 const IndexPage = () => (
@@ -15,11 +23,11 @@ const IndexPage = () => (
                 <Link to="#">click me</Link>
                 <div className="Logos">
                     <img src={require('../images/logo-xcode.png')} width="50" />
-                    <img src={require('../images/logo-figma.png')} width="50" />
-                    <img src={require('../images/logo-framer.png')} width="50" />
-                    <img src={require('../images/logo-invision.png')} width="50" />
-                    <img src={require('../images/logo-sketch.png')} width="50" />
-                    <img src={require('../images/logo-swift.png')} width="50" />
+                    <img src={figmalogo} width="50" />
+                    <img src={framerlogo} width="50" />
+                    <img src={invisionlogo} width="50" />
+                    <img src={sketchlogo} width="50" />
+                    <img src={swiftlogo} width="50" />
                 </div>
                 <svg width="100%" height="172" file="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill="white">
@@ -35,7 +43,31 @@ const IndexPage = () => (
                 </svg>
             </div>
         </div>
-        
+        <div className="Cards">
+            <h2>11 courses, more coming</h2>
+            <div className="CardGroup">
+            <Card 
+                title="React for Designers" 
+                text="12 section"
+                image={require('../images/wallpaper2.jpg')}
+            />
+            <Card 
+                title="Swift for Designers" 
+                text="12 section"
+                image={require('../images/wallpaper3.jpg')}
+            />
+            <Card 
+                title="Vue for Designers" 
+                text="12 section"
+                image={require('../images/wallpaper2.jpg')}
+            />
+            <Card 
+                title="JS for Designers" 
+                text="12 section"
+                image={require('../images/wallpaper3.jpg')}
+            />
+            </div>
+        </div>
     </div>
 )
 
